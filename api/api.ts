cdb854935d33abbd344ddbdb97360686734154a6
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2023-10-29 00:05:16 -0300

    initial commmit

---
+++ api/api.ts
@@ -0,0 +1,76 @@
+export const desp = [
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+  {
+    purchase: "alguma coisa ",
+    price: "400 R$ ",
+    by: "fulano",
+    date: "11/11/11",
+  },
+];
